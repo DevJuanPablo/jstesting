1cde3ad2a0ee0acfbbeb5ea7c2aa8f4e27bf5ded
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-07 23:43:59 -0400

    test con variables primitivas de tipo Boolean

--- testing/__tests__/index.test.js
+++ testing/__tests__/index.test.js
@@ -1,11 +1,5 @@
-test('Quiero verificar que 2 + 2 = 4', () => {
-  expect(2 + 2).toBe(4)
-})
+var holaMundo = 'Hola Mundo'
 
-test('Quiero verificar que 2+2=4 pero usando toEqual()', () => {
-  expect(2 + 2).toEqual(4)
-})
-
-test('Quiero verificar que 5 es mayor 1u3 4', () => {
-  expect(5).toBeGreaterThan(4)
+test('Que la palabra hola este dentro de mi variable holaMundo', () => {
+  expect(holaMundo).not.toMatch(/cola/)
 })
