ba4e772ad82649febdc46e4fedd21d855f8e7464
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-08 21:40:00 -0400

    test con Promesas

--- testing/__tests__/index.test.js
+++ testing/__tests__/index.test.js
@@ -1,8 +1,16 @@
-const palindrome = word => word
-  .split('')
-  .reverse()
-  .join('')
+const palindrome = word => {
+  return new Promise((resolve, reject) => {
+    if (!word) reject('La palabra esta vacia')
+    
+    const palabraInvertida = word.split('').reverse().join('')
+
+    resolve(palabraInvertida)
+  }) 
+}
 
 test('Quiero verificar que hola es igual a aloh', () => {
-  expect(palindrome('hola')).toBe('alo')
+  return palindrome('hola')
+    .then(str => {
+      expect(str).toBe('aloh')
+    })
 })
