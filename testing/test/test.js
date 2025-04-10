415e2ba03c3d74eb77481d6c35cbe3bcfc3f14e2
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-09 23:05:49 -0400

    test chai - array

--- testing/test/test.js
+++ testing/test/test.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert
+const should = require('chai').should()
 
 describe('Mis ejemplos con chai', () => {
   it('validando que el numero 5 este en mi array', () => {
@@ -8,5 +9,11 @@ describe('Mis ejemplos con chai', () => {
   it('validar que 2 + 2 = 4', () => {
     const suma = 2 + 2
     assert.equal(suma, 4)
+  })
+
+  it('validar la cantidad de elementos', () => {
+    const array = [10, 20, 30, 40]
+
+    array.should.have.lengthOf(4)
   })
 })
