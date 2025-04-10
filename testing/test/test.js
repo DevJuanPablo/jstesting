199edb29d5bc7f2c2ec731fc6b7a11651eb47e62
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-09 22:00:51 -0400

    test chai - variables de tipo numero

--- testing/test/test.js
+++ testing/test/test.js
@@ -2,6 +2,11 @@ const assert = require('chai').assert
 
 describe('Mis ejemplos con chai', () => {
   it('validando que el numero 5 este en mi array', () => {
-    assert.equal([1, 2, 3, 4, 5, 6].indexOf(0), 4)
+    assert.equal([1, 2, 3, 4, 5, 6].indexOf(0), -1)
+  })
+
+  it('validar que 2 + 2 = 4', () => {
+    const suma = 2 + 2
+    assert.equal(suma, 4)
   })
 })
